dcd2a9f6f5fe4caede7471693c4019d5d1bf48a1
Rizki <106717697+RizqGyx@users.noreply.github.com>
2023-10-24 01:01:51 +0700

    Third Commit

--- js/menuProduct.js
+++ js/menuProduct.js
@@ -2,15 +2,34 @@ import { Card } from './components/cardUpdate.js'
 
 window.addEventListener("DOMContentLoaded", () => {
     const sundae = document.getElementById("sundae-item");
-    const eskrim = document.getElementById("eskrim");
+    const eskrim = document.getElementById("eskrim-item");
+    const smoothies = document.getElementById("smoothies-item");
+    const coffee = document.getElementById("coffee-item");
 
-
-    console.log(Card('sundae.jpg', 'Sundae', '16.000', 'Soft and delicious Vanilla ice cream cone'));
-    sundae.innerHTML += Card('sundae/sundae.jpg', 'Sundae', '16.000', 'Soft and delicious Vanilla ice cream cone');
-    sundae.innerHTML += Card('sundae/Boba-Sundae.jpg', 'Boba Sundae', '16.000', 'Soft and delicious Vanilla ice cream cone');
-    sundae.innerHTML += Card('sundae/Berry-Bean-Sundae.jpg', 'Berry Bean Sundae', '16.000', 'Ice Cream combined with soft Red Bean and sweet Strawberry jam');
-    sundae.innerHTML += Card('sundae/Chocolate-Lucky-Sundae.jpg', 'Chocolate Lucky Sundae', '16.000', 'Ice Cream with Chocolate Sauce and a surprise Topping underneath');
-    sundae.innerHTML += Card('sundae/Mango-Sundae.jpg', 'Mango Sundae', '16.000', 'Vanilla Ice Cream with real Mango Slice Topping');
-    sundae.innerHTML += Card('sundae/Mixed-Sundae-Vanilla-Strawberry-Mixue.jpg', 'Mixed Sundae V&S', '16.000', 'A delightful blend of creamy Vanilla and luscious Strawberry Ice Cream');
-    sundae.innerHTML += Card('sundae/Oreo-Sundae.jpg', 'Mixed Sundae V&S', '16.000', 'A delightful blend of creamy Vanilla and luscious Strawberry Ice Cream');
+    sundae.innerHTML += Card('Sundae/sundae.jpg', 'Sundae', '16.000', 'Soft and delicious Vanilla ice cream cone');
+    sundae.innerHTML += Card('Sundae/Boba-Sundae.jpg', 'Boba Sundae', '16.000', 'Ice Cream with Brown Sugar Syrup Sauce and Chewy Boba Topping');
+    sundae.innerHTML += Card('Sundae/Berry-Bean-Sundae.jpg', 'Berry Bean Sundae', '16.000', 'Ice Cream combined with soft Red Bean and sweet Strawberry jam');
+    sundae.innerHTML += Card('Sundae/Chocolate-Lucky-Sundae.jpg', 'Chocolate Lucky Sundae', '16.000', 'Ice Cream with Chocolate Sauce and a surprise Topping underneath');
+    sundae.innerHTML += Card('Sundae/Mango-Sundae.jpg', 'Mango Sundae', '16.000', 'Vanilla Ice Cream with real Mango Slice Topping');
+    sundae.innerHTML += Card('Sundae/Mixed-Sundae-Vanilla-Strawberry-Mixue.jpg', 'Mixed Sundae V&S', '16.000', 'A delightful blend of creamy Vanilla and luscious Strawberry Ice Cream');
+    sundae.innerHTML += Card('Sundae/Oreo-Sundae.jpg', 'Oreo Sundae', '16.000', 'Ice Cream with chocolate and crunchy Oreo sprinkles');
+    sundae.innerHTML += Card('Sundae/Strawberry-Lucky-Sundae.jpg', 'Strawberry Lucky Sundae', '16.000', 'Ice Cream with Strawberry sauce, fresh Strawberry chunks, and a surprise topping underneath');
+    sundae.innerHTML += Card('Sundae/Strawberry-Sundae-Mixue.jpg', 'Strawberry Sundae', '16.000', 'Smooth and delicious Strawberry ice cream');
+    
+    eskrim.innerHTML += Card('Ice Cream/Mixed-ice-cream-Vanilla-Strawberry-Mixue-225x300.jpg', 'Mixed Ice Cream', '8.000', 'Mixed Vanilla and Strawberry Cone Ice Cream, a soft and delightful combination');
+    eskrim.innerHTML += Card('Ice Cream/strawberry-ice-cream-mixue.jpg', 'Strawberry Ice Cream', '8.000', 'Strawberry Cone Ice Cream, a soft and delightful treat');
+    eskrim.innerHTML += Card('Ice Cream/vanila-ice-cream-mixue.jpg', 'Vanila Ice Cream', '8.000', 'Vanilla Cone Ice Cream, a soft and delightful treat');
+    
+    smoothies.innerHTML += Card('Smoothies/Chocolate-Cookies-Smoothies-with-Ice-Cream-300x300.jpg', 'Coffee Ice Cream', '8.000', 'Coffee Ice Cream, a creamy delight infused with delicious coffee flavor');
+    smoothies.innerHTML += Card('Smoothies/Kiwi-Smoothies-mixue.jpg', 'Coffee Ice Cream', '8.000', 'Coffee Ice Cream, a creamy delight infused with delicious coffee flavor');
+    smoothies.innerHTML += Card('Smoothies/Mango-Smoothies-With-Ice-Cream.jpg', 'Coffee Ice Cream', '8.000', 'Coffee Ice Cream, a creamy delight infused with delicious coffee flavor');
+    smoothies.innerHTML += Card('Smoothies/Strawberry-Smoothies-With-Ice-Cream.jpg', 'Coffee Ice Cream', '8.000', 'Coffee Ice Cream, a creamy delight infused with delicious coffee flavor');
+    
+    coffee.innerHTML += Card('Coffee/Coffee-Boba-Sundae.jpg', 'Coffee Boba Sundae', '18.000', 'Coffee Boba Sundae, coffee ice cream enhanced with chewy boba toppings for added indulgence');
+    coffee.innerHTML += Card('Coffee/Coffee-Cookies-Sundae.jpg', 'Coffee Cookies Sundae', '18.000', 'Coffee Cookies Sundae, coffee ice cream enhanced with cookie toppings for added delight');
+    coffee.innerHTML += Card('Coffee/Coffee-Ice-Cream.jpg', 'Coffee Ice Cream', '8.000', 'Coffee Ice Cream, a creamy delight infused with delicious coffee flavor');
+    coffee.innerHTML += Card('Coffee/Coffee-Smoothies.jpg', 'Coffee Smoothies', '16.000', 'Enjoyable Coffee Smoothies with added ice cream for extra delight');
+    coffee.innerHTML += Card('Coffee/Lemon-Coffee.jpg', 'Lemon Coffee', '14.000', 'Lemon Coffee, a new coffee product with a hint of lemon for added freshness');
+    coffee.innerHTML += Card('Coffee/Mixue-Americano-Coffee.jpg', 'Americano Coffee', '12.000', 'Delicious Americano Coffee');
+    coffee.innerHTML += Card('Coffee/Mocha-Mi-Shake.jpg', 'Mocha Mi Shake', '16.000', 'Refreshing and delightful Mocha Mi-Shake');
 });
